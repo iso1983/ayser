102864c19cd5d3433a0a449365ea9fb39275380f
iso <iso@MacBook-Pro.local>
2019-01-25 20:18:07 +0200

    fist

---
+++ precache-manifest.fd082ba84d0fde9ab565bd5a53baa945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27a7f35cb777739bc98b",
+    "url": "/static/css/main.bfb919d7.chunk.css"
+  },
+  {
+    "revision": "27a7f35cb777739bc98b",
+    "url": "/static/js/main.27a7f35c.chunk.js"
+  },
+  {
+    "revision": "c6f9d0d2245551fb674e",
+    "url": "/static/js/1.c6f9d0d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b7881addb21cd88f84ce5abdf6d4d90",
+    "url": "/index.html"
+  }
+];
